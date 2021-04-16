b06c199e50944e47aa5f68825a1f3425e7565645
unknown <mgp.ballat@gmail.com>
2021-04-16 18:29:17 +0200

    react-router installed

--- my-app/src/components/App/index.js
+++ my-app/src/components/App/index.js
@@ -1,13 +1,24 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import './style.scss';
 
 import Header from '../Header';
 import Home from '../Home';
+import Street from '../Street';
 
 function App() {
 	return (
 		<div className='app'>
 			<Header />
-			<Home />
+			<Switch>
+				<Route path='/' exact>
+					<Home />
+				</Route>
+				<Route path='/street' exact>
+					<Street />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
---
+++ my-app/src/components/Street/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './style.scss';
+
+const Street = () => <div>Street</div>;
+
+export default Street;
