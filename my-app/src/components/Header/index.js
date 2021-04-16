bba4c95e68549652bccba53f9bd5072e5052891d
unknown <mgp.ballat@gmail.com>
2021-04-16 21:15:20 +0200

    header done

--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './style.scss';
 
+import fbImage from '../../images/facebook.png';
+import instaImage from '../../images/instagram.png';
+import twitterImage from '../../images/twitter.png';
+import youtubeImage from '../../images/youtube.png';
+
 const Header = () => (
 	<div className='header'>
 		<h1 className='header_title'>âme</h1>
@@ -32,6 +37,12 @@ const Header = () => (
 				</li>
 			</ul>
 		</nav>
+		<div className='header_social-network'>
+			<img src={fbImage} alt='facebook' className='logo-social' />
+			<img src={instaImage} alt='instagram' className='logo-social' />
+			<img src={twitterImage} alt='twitter' className='logo-social' />
+			<img src={youtubeImage} alt='youtube' className='logo-social' />
+		</div>
 		<div className='header_footer'>
 			<p className='header_footer-date'> 2021</p>
 			<p className='header_footer-details'>détails</p>
